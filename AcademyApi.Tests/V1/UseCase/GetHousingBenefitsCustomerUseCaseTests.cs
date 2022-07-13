6d7afdc4b996e85b003fabf174c70ab5a8e80311
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-07-13 11:02:25 +0100

    Refactor Housing Benefits use case test

--- AcademyApi.Tests/V1/UseCase/GetHousingBenefitsCustomerUseCaseTests.cs
+++ AcademyApi.Tests/V1/UseCase/GetHousingBenefitsCustomerUseCaseTests.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AcademyApi.V1.Boundary.Response;
 using AcademyApi.V1.Gateways.Interfaces;
@@ -9,7 +8,6 @@ using FluentAssertions;
 using Hackney.Core.Testing.Shared;
 using Moq;
 using NUnit.Framework;
-using Address = AcademyApi.V1.Boundary.Address;
 
 namespace AcademyApi.Tests.V1.UseCase;
 
@@ -17,6 +15,7 @@ public class GetHousingBenefitsCustomerUseCaseTests : LogCallAspectFixture
 {
     private Mock<IHousingBenefitsGateway> _mockGateway;
     private GetHousingBenefitsCustomerUseCase _classUnderTest;
+    private Fixture _fixture = new();
 
     [SetUp]
     public void Setup()
@@ -28,42 +27,21 @@ public class GetHousingBenefitsCustomerUseCaseTests : LogCallAspectFixture
     [Test]
     public async Task ExecuteReturnsHousingBenefitsRecord()
     {
-        var stubBenefitsResponseObject = new BenefitsResponseObject
-        {
-            ClaimId = 5260765,
-            CheckDigit = "6",
-            PersonReference = 1,
-            Title = "Ms",
-            FirstName = "Elwira",
-            LastName = "Moncur",
-            DateOfBirth = new DateTime(1971, 12, 22),
-            FullAddress = new Address()
-            {
-                Line1 = "6 Cascade Junction",
-                Line2 = "49 Norway Maple Pass",
-                Line3 = "LONDON",
-                Line4 = null,
-                Postcode = "I3 0RP",
-            },
-            PostCode = "I3 0RP",
-            HouseholdMembers = new List<HouseHoldMember>()
-            {
-                new()
-                {
-                    Title = "Ms",
-                    FirstName = "Elwira",
-                    LastName = "Moncur",
-                    DateOfBirth = new DateTime(1971, 12, 22),
-                }
-            },
-            Benefits = null
-        };
-        var claimId = stubBenefitsResponseObject.ClaimId;
-        var personRef = stubBenefitsResponseObject.PersonReference;
+        var claimId = _fixture.Create<int>();
+        var personReference = _fixture.Create<int>();
+        var stubBenefitsResponseObject = _fixture.Build<BenefitsResponseObject>()
+            .With(o => o.ClaimId, claimId)
+            .With(o => o.PersonReference, personReference)
+            .Without(o => o.Benefits)
+            .Create();
+        var stubBenefits = _fixture.CreateMany<Benefits>().ToList();
         _mockGateway.Setup(x =>
-            x.GetCustomer(claimId, personRef)).ReturnsAsync(stubBenefitsResponseObject);
+            x.GetCustomer(claimId, personReference)).ReturnsAsync(stubBenefitsResponseObject);
+        _mockGateway.Setup(x =>
+            x.GetBenefits(claimId)).ReturnsAsync(stubBenefits);
 
-        var response = await _classUnderTest.Execute($"{claimId}{GetHousingBenefitsCustomerUseCase.IdSeparator}{personRef}");
+        var response = await _classUnderTest
+            .Execute($"{claimId}{GetHousingBenefitsCustomerUseCase.IdSeparator}{personReference}");
 
         response.Should().BeEquivalentTo(stubBenefitsResponseObject);
     }
