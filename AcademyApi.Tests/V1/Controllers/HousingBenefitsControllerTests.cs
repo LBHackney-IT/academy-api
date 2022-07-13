48befdbee6237636ebc9b36b4243d415b3f75302
Jonathan Lewis <jonathan.lewis@madetech.com>
2022-07-13 11:58:39 +0100

    Add Housing Benfits controller tests

--- AcademyApi.Tests/V1/Controllers/HousingBenefitsControllerTests.cs
+++ AcademyApi.Tests/V1/Controllers/HousingBenefitsControllerTests.cs
@@ -1,7 +1,10 @@
+using AcademyApi.V1.Boundary.Response;
 using AcademyApi.V1.Controllers;
 using AcademyApi.V1.UseCase.Interfaces;
 using AutoFixture;
+using FluentAssertions;
 using Hackney.Core.Testing.Shared;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 
@@ -43,6 +46,31 @@ namespace AcademyApi.Tests.V1.Controllers
             _classUnderTest.ViewRecord(benefitsId);
 
             _mockGetCustomerUseCase.Verify(x => x.Execute(benefitsId), Times.Once);
+        }
+
+        [Test]
+        public void ViewRecordReturnsSuccessResponse()
+        {
+            var benefitsId = _fixture.Create<string>();
+            var stubBenefitsResponseObject = _fixture.Create<BenefitsResponseObject>();
+            _mockGetCustomerUseCase.Setup(x =>
+                x.Execute(benefitsId)).ReturnsAsync(stubBenefitsResponseObject);
+
+            var response = _classUnderTest.ViewRecord(benefitsId);
+
+            response.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Test]
+        public void ViewRecordReturnsNotFoundResponse()
+        {
+            var benefitsId = _fixture.Create<string>();
+            _mockGetCustomerUseCase.Setup(x =>
+                x.Execute(benefitsId)).ReturnsAsync((BenefitsResponseObject) null);
+
+            var response = _classUnderTest.ViewRecord(benefitsId);
+
+            response.Should().BeOfType<NotFoundObjectResult>();
         }
     }
 }
