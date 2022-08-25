c9ed76535adcf4e6cc20ee7286bda4e7b691277f
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-08-25 14:26:01 +0100

    added gateway to get landlord details

---
+++ AcademyApi/V1/Domain/HousingBenefitLandlordDetails.cs
@@ -0,0 +1,13 @@
+namespace AcademyApi.V1.Domain;
+
+public class HousingBenefitLandlordDetails
+{
+    public int ClaimId { get; set; }
+    public string Name { get; set; }
+    public string Addr1 { get; set; }
+    public string Addr2 { get; set; }
+    public string Addr3 { get; set; }
+    public string Addr4 { get; set; }
+    public string Postcode { get; set; }
+    public int CreditorId { get; set; }
+}
