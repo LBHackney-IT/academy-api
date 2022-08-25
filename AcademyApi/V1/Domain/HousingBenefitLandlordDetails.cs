d62bbaa01082ab9002e8487a79d2e284cf944b35
Charli Wild <charli.wild@Charlis-MacBook-Pro.local>
2022-08-25 14:43:32 +0100

    added nullable to address fields

--- AcademyApi/V1/Domain/HousingBenefitLandlordDetails.cs
+++ AcademyApi/V1/Domain/HousingBenefitLandlordDetails.cs
@@ -4,10 +4,14 @@ public class HousingBenefitLandlordDetails
 {
     public int ClaimId { get; set; }
     public string Name { get; set; }
-    public string Addr1 { get; set; }
-    public string Addr2 { get; set; }
-    public string Addr3 { get; set; }
-    public string Addr4 { get; set; }
-    public string Postcode { get; set; }
+
+    #nullable enable
+    public string? Addr1 { get; set; }
+    public string? Addr2 { get; set; }
+    public string? Addr3 { get; set; }
+    public string? Addr4 { get; set; }
+    public string? Postcode { get; set; }
+
+    #nullable disable
     public int CreditorId { get; set; }
 }
